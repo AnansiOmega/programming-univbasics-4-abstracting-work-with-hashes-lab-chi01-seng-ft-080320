7382cf2024ca65c33290f61db58f38a317632e22
Ignas Butautas <ignasbutautas7@gmail.com>
2020-07-17 19:16:29 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -9,7 +9,7 @@ end
 def update_counting_hash(hash, key)
 if hash[key]
   hash[key] += 1
-else hash[:1] = key
+else hash[key] = 1
 end
 
   # given a hash an a key as parameters, return an updated hash

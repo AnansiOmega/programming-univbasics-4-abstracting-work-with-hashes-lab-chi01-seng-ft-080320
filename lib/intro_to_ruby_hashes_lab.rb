6700c6279fd60cc04a7c69d215ff6370218b664d
Ignas Butautas <ignasbutautas7@gmail.com>
2020-07-17 19:09:48 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -11,7 +11,6 @@ if hash[key]
   hash[key] += 1
 else hash[1]
 end
-end
 
   # given a hash an a key as parameters, return an updated hash
   # if the provided key is not present in the hash, add it and assign it to the value of 1
